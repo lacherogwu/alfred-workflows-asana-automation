acd173ab41d1548bb92c2825525a16bcc3f46802
Asaf <asaf@utopia.tech>
2022-03-24 16:38:43 +0200

    fixed sync projects

--- src/projects.js
+++ src/projects.js
@@ -5,30 +5,35 @@ const { close: subtitle } = require('./text.json');
 
 const sync = process.argv.pop() === 'sync';
 
-const syncProjects = async () => {
-    const response = await promise(client.projects.getProjects({ workspace: process.env.workspace }));
-    const data = response.data.map(proj => ({
-        title: proj.name,
-        subtitle: proj.gid,
-        arg: proj.gid
-    }));
-    fs.writeFile('projects.json', JSON.stringify(data), () => {});
+const syncProjects = async (items = [], offset) => {
+	const response = await promise(client.projects.getProjects({ workspace: process.env.workspace, limit: 100, offset }));
+	const offsetToken = response._response?.next_page?.offset;
 
-    return data;
+	items = [...items, ...response.data];
+	if (offsetToken) return syncProjects(items, offsetToken);
+
+	const data = items.map(proj => ({
+		title: proj.name,
+		subtitle: proj.gid,
+		arg: proj.gid,
+	}));
+	fs.writeFile('projects.json', JSON.stringify(data), () => {});
+
+	return data;
 };
 
 (async () => {
-    if(sync){
-        await syncProjects();
-        output({ title: 'Projects synced successfully!', subtitle });
-    } else {
-        const file = checkFile('projects.json', '[]');
-        if(file.length){
-            output(file);
-        } else {
-            // write the file
-            const data = await syncProjects();
-            output(data);
-        }
-    }
+	if (sync) {
+		await syncProjects();
+		output({ title: 'Projects synced successfully!', subtitle });
+	} else {
+		const file = checkFile('projects.json', '[]');
+		if (file.length) {
+			output(file);
+		} else {
+			// write the file
+			const data = await syncProjects();
+			output(data);
+		}
+	}
 })();
