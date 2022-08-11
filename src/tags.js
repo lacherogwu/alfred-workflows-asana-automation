180951f31df65965479ac37c9f08e9c255b3e336
Venkatraman D <venkat@flocations.com>
2022-08-11 17:05:40 +0530

    Added tags option

--- src/tags.js
+++ src/tags.js
@@ -1,18 +1,3 @@
-// const client = require('./api');
-// const { output, promise } = require('./utils');
-// const { copy: subtitle } = require('./text.json');
-
-// (async () => {
-//     const response = await promise(client.tags.findByWorkspace(process.env.workspace));
-//     const data = response.data.map(i => ({
-//         title: `${i.name} - ${i.gid}`,
-//         subtitle,
-//         arg: i.gid
-//     }));
-//     output(data);
-// })();
-
-
 const client = require('./api');
 const fs = require('fs');
 const { output, promise, checkFile } = require('./utils');
